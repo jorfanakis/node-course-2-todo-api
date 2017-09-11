9165b1200877160b4e048606a85d84af5d063a83
Josh Orfanakis <jorfanakis@planetdata.com>
2017-09-11 12:05:13 -0600

    Added test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 // Dummy todos to seed db
 const todos = [
-  { text: 'First test todo'},
-  { text: 'Second test todo'}
+  { _id: new ObjectID(),
+    text: 'First test todo'},
+  { _id: new ObjectID(),
+    text: 'Second test todo'}
 ];
 
 // Run before each test
@@ -70,4 +73,31 @@ describe('GET /todos', () => {
        })
       .end(done);
   });
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if not found', (done) => {
+    // Make sure 404 is returned.
+    request(app)
+      .get(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
 });
