5a8a71e7c2450279214f4d524645d5928ea08a85
Josh Orfanakis <jorfanakis@planetdata.com>
2017-09-11 10:59:21 -0600

    Add GET /todos/id

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose.js');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -32,6 +33,23 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+
+  }).catch((e) => { res.send(400).send(); });
 });
 
 app.listen(3000, () => {
